8be7c3489b1f284ac703b60412109d4ca01db24f
Denis Mosin <mosin-denis@bk.ru>
2025-07-02 23:01:45 +0700

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
-
 
 database = [
     {"login": "admin", "password": "1234"},
@@ -9,44 +9,36 @@ database = [
     {"login": "denis", "password": "lol"}
 ]
 
-
 class UserSchema(BaseModel):
     login: str
     password: str
 
-
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/login", tags=["Authorization"])
 async def login(data: UserSchema):
     for user in database:
         if user["login"] == data.login:
             if user["password"] == data.password:
-                return {"status": "ok"}
-            raise HTTPException(
-                status_code=401,
-                detail="Incorrect password",
-                headers={"WWW-Authenticate": "Bearer"}
-            )
-    raise HTTPException(
-        status_code=404,
-        detail="User not found",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-
+                return {"status": "ok", "login": data.login}
+            raise HTTPException(status_code=401, detail="Incorrect password")
+    raise HTTPException(status_code=404, detail="User not found")
 
 @app.post("/register", tags=["Authorization"])
 async def register(data: UserSchema):
     if any(user["login"] == data.login for user in database):
-        raise HTTPException(
-            status_code=400,
-            detail="User already exists"
-        )
-
+        raise HTTPException(status_code=400, detail="User already exists")
     database.append({"login": data.login, "password": data.password})
-    return {"status": "ok"}
-
+    return {"status": "ok", "login": data.login}
 
 @app.get("/users", tags=["Users"])
 async def get_users() -> List[dict]:
