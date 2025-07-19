222fc9b1dc1861ab4311552712fbb28aef6640fe
Denis Mosin <mosin-denis@bk.ru>
2025-07-20 04:34:38 +0700

    add like system

--- frontend/src/components/pages/PostsPage.tsx
+++ frontend/src/components/pages/PostsPage.tsx
@@ -2,6 +2,7 @@ import Header from "../items/Header.tsx";
 import CreatePost from "../items/CreatePost.tsx";
 import axios from 'axios'
 import {useEffect, useState} from "react";
+import {useAuthStore} from "../store/AuthStore.ts";
 
 interface Post {
     title: string;
@@ -11,7 +12,26 @@ interface Post {
 }
 
 export default function PostsPage() {
+    const {isAuthenticated} = useAuthStore();
     const [posts, setPosts] = useState<Post[]>([]);
+    const [islike, setIsLike] = useState(false);
+    const savelike = async (post: Post) => {
+            try {
+                const responce = await axios.put('http://127.0.0.1:8000/like', {
+                    title: post.title,
+                    content: post.content,
+                    user: post.user,
+                    likes: post.likes,
+                    islike: islike
+                })
+                console.log(responce);
+                setIsLike(!islike);
+            } catch (error) {
+                console.error(error)
+            }
+
+
+    }
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -29,23 +49,32 @@ export default function PostsPage() {
     return (
         <div className="animate-fadeIn min-h-screen bg-gray-100">
             <Header/>
-            <div className="container mx-auto px-4 py-8">
-                <CreatePost/>
-                <div className="mt-8 space-y-6">
-                    {posts.map((post, index) => (
-                        <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
-                            <div className="p-6">
-                                <h3 className="text-xl font-bold text-gray-800 mb-2">
-                                    <span>{post.user}</span> {post.title}</h3>
-                                <p className="text-gray-600 mb-4">{post.content}</p>
-                                <div className="flex justify-between text-sm text-gray-500">
-                                    <span> <img src="/icons/heart.svg" alt=""/>{post.likes}</span>
+            {isAuthenticated ?
+                <div className="container mx-auto px-4 py-8">
+                    <CreatePost/>
+                    <div className="mt-8 space-y-6">
+                        {posts.map((post, index) => (
+                            <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
+                                <div className="p-6">
+                                    <h3 className="text-xl font-bold text-gray-800 mb-2">
+                                        <span>{post.user}</span> {post.title}</h3>
+                                    <p className="text-gray-600 mb-4">{post.content}</p>
+                                    <div className="flex justify-between text-sm text-gray-500">
+                                        <button onClick={() => savelike(post)}><span> <img src="/icons/heart.svg"
+                                                                                           alt=""/>{post.likes}</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+                </div> :
+                <div>
+                    Авторизируйся чтобы видеть посты других пользователей
                 </div>
-            </div>
+            }
+
+
         </div>
     )
 }
