a65f51989118cfcc55d0f3a319c2dd481e9ce37a
Denis Mosin <cloudy1524@gmail.com>
2025-08-28 11:04:40 +0300

    add bundler analytic, install tanstack query

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 export default defineConfig({
   root: "client",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      filename: "stats.html",
+    }),
+  ],
   resolve: {
     alias: {
       client: path.resolve(__dirname, "client"),
